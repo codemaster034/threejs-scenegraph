2d5ce5f97bc9eabd242046150f2d1cef3740c01b
codemaster034 <codemaster034@gmail.com>
2022-05-24 00:49:50 +0900

    Threejs tank example

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Example from "./example";
+// import SceneGraph from "./scenegraph";
+
+function App() {
+  return (
+    <>
+      <Example />
+      {/* <SceneGraph /> */}
+    </>
+  );
+}
+
+export default App;
